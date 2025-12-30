78c466748a07e1df6fde6c2da055cbc4f969803b
syful islam siyam <133095845+siaym@users.noreply.github.com>
2025-12-31 02:36:29 +0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,15 +1,18 @@
-// firebase.js (PUBLIC SITE VERSION)
+// firebase.js (UPDATED WITH REAL KEYS)
 
 const firebaseConfig = {
-  apiKey: "...",
-  authDomain: "...",
-  projectId: "...",
-  storageBucket: "...",
-  messagingSenderId: "...",
-  appId: "..."
+  apiKey: "AIzaSyARRuy_LK77TV9_xk7qngO6F99Bf63TmjE",
+  authDomain: "my-protfolio-36077.firebaseapp.com",
+  projectId: "my-protfolio-36077",
+  storageBucket: "my-protfolio-36077.firebasestorage.app",
+  messagingSenderId: "889602371178",
+  appId: "1:889602371178:web:1afebab03b1f1e6bf5a413"
 };
 
-firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
-// ONLY Firestore here
+// Export Firestore database
 const db = firebase.firestore();
